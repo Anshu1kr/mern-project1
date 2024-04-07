d80827c08ea1556f4b156822fc0087f51ebac63e
Anshu Kumar <anshu6kr@gmail.com>
2024-04-07 22:54:09 +0530

    creating and organizing routes

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
+const router = require("./router/auth-router");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome to mern stack project");
-});
-
-app.get("/register", (req, res) => {
-  res.status(200).send("Welcome to mern stack registration page");
-});
+// Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
+app.use("/api/auth", router);
 
 const PORT = 5000;
 app.listen(PORT, () => {
