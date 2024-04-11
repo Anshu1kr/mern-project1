27b3d86402840a8592ca31214dbe4a0c1432426b
Anshu Kumar <anshu6kr@gmail.com>
2024-04-11 12:19:59 +0530

    modifying the user schema

--- models/user-model.js
+++ models/user-model.js
@@ -17,10 +17,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  username: {
-    type: String,
-    required: true,
-  },
   isAdmin: {
     type: Boolean,
     default: false,
