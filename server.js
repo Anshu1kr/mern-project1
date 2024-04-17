0cd84005d8fea98d4f84bfb1bd1915e4142a7f76
Anshu Kumar <anshu6kr@gmail.com>
2024-04-17 21:02:20 +0530

    building complete contact

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const router = require("./router/auth-router");
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
@@ -14,7 +15,8 @@ beginning of your middleware stack to ensure it's available for all subsequent r
 */
 
 // Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
-app.use("/api/auth", router);
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
 
 app.use(errorMiddleware);
 
